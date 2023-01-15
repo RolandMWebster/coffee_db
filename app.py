0fffee42af929f597ecea6aec33e20b2f278967a
Ned Webster <ned.webster@simplybusiness.co.uk>
2023-01-15 13:42:48 +0000

    code insert coffee feature

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 
 import pandas as pd
-import psycopg2
 import streamlit as st
 
 from coffee_db import CoffeeDB
@@ -11,3 +10,13 @@ db = CoffeeDB()
 
 st.title("Hello Coffee World!")
 st.dataframe(data=db.get_data())
+
+st.header("Add Coffee")
+form = st.form(key="add_coffee")
+with form:
+    coffee_id = st.text_input("id")
+    roastery = st.text_input("Roastery")
+    country = st.text_input("Country")
+    submit = st.form_submit_button("Add")
+    if submit:
+        db.insert_coffee((coffee_id, roastery, country,))
