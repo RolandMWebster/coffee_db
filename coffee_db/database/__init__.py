0c67ceba65c29ff931f5a47f390db8f89c15b937
Ned Webster <49588411+nedwebster@users.noreply.github.com>
2023-01-16 17:19:01 +0000

    stop running on config.py if not running locally (#5)

--- coffee_db/database/__init__.py
+++ coffee_db/database/__init__.py
@@ -2,8 +2,7 @@ import os
 
 from coffee_db.database.config import config
 
-
-DATABASE_URL = os.environ.get(
-    "DATABASE_URL",
-    config(),
-)
+if "DATABASE_URL" in os.environ.keys():
+    DATABASE_URL = os.environ["DATABASE_URL"]
+else:
+    DATABASE_URL = config()
