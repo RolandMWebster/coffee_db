c75aca659cbec2180231d862c13055e1d0ea876d
Roland <rolandmwebster@gmail.com>
2023-01-16 08:53:05 -0800

    Refactor for new data loader

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import copy
 
 import streamlit as st
 from streamlit_folium import folium_static
+import pandas as pd
 
 from coffee_db import CoffeeDB
-from coffee_db.coffee import Coffee, Roastery, Country
+from coffee_db.data_loaders import PostgresDataLoader
 from coffee_db.visualizations.world_map_visualizer import MapVisualizer
 
 
@@ -14,12 +14,16 @@ def main():
 
     folium_static(plot_map())
 
-    st.dataframe(coffees)
-    st.dataframe(roasteries)
-    st.dataframe(countries)
+    add_tables()
 
     add_forms()
     remove_forms()
+
+
+def add_tables():
+    st.dataframe(pd.DataFrame([dict(coffee) for coffee in coffees]))
+    st.dataframe(pd.DataFrame([dict(roastery) for roastery in roasteries]))
+    st.dataframe(pd.DataFrame([dict(country) for country in countries]))
 
 
 def add_forms():
@@ -45,16 +49,15 @@ def remove_forms():
     with col3:
         remove_country_form()
 
-
 def add_coffee_form():
     st.header("Add Coffee")
     with st.form(key="add_coffee", clear_on_submit=True):
         name = st.text_input("Name")
         country_of_origin = st.selectbox(
-            "Country of Origin", (x["name"] for x in countries)
+            "Country of Origin", (country.name for country in countries)
         )
         roastery = st.selectbox(
-            "Roastery", (x["name"] for x in roasteries)
+            "Roastery", (roastery.name for roastery in roasteries)
         )
         elevation = st.text_input("Elevation")
         submit = st.form_submit_button("Add")
@@ -88,7 +91,7 @@ def add_roastery_form():
     with st.form(key="add_roastery", clear_on_submit=True):
         name = st.text_input("Name")
         country = st.selectbox(
-            "Country", (x["name"] for x in countries)
+            "Country", (country.name for country in countries)
         )
         submit = st.form_submit_button("Add")
         if submit:
@@ -137,34 +140,14 @@ def remove_country_form():
 def plot_map():
 
     visualizer = MapVisualizer()
-    
-    countries_copy = copy.deepcopy(countries)
-    country_dict = {}
-    for x in countries_copy:
-        country_dict[x["name"]] = Country(**x)
-    
-    roasteries_copy = copy.deepcopy(roasteries)
-    roastery_dict = {}
-    for x in roasteries_copy:
-        x["country"] = country_dict[x["country"]]
-        roastery_dict[x["name"]] = Roastery(**x)
-    
-    coffees_copy = copy.deepcopy(coffees)
-    coffee_dict = {}
-    for x in coffees_copy:
-        x["roastery"] = roastery_dict[x["roastery"]]
-        x["country_of_origin"] = country_dict[x["country_of_origin"]]
-        coffee_dict[x["name"]] = Coffee(**x)
-
-    return visualizer.plot_coffees_by_country(list(coffee_dict.values()))
+    return visualizer.plot_coffees_by_country(coffees)
 
 
 if __name__ == "__main__":
 
     db = CoffeeDB()
+    data_loader = PostgresDataLoader(db=db)
 
-    coffees = db.get_data("coffee")
-    roasteries = db.get_data("roastery")
-    countries = db.get_data("country")
+    countries, roasteries, coffees = data_loader.get_data()
 
     main()
