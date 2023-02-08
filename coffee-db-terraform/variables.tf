02c9c360d6177b38f27cc2235f5e29effda074a1
Ned Webster <edwardpwebster@gmail.com>
2023-02-08 21:34:02 +0000

    try to add rds db

--- coffee-db-terraform/variables.tf
+++ coffee-db-terraform/variables.tf
@@ -7,3 +7,13 @@ variable "aws_secret_key" {
   type = string
   sensitive = true
 }
+
+variable "rds_username" {
+  type = string
+  sensitive = true
+}
+
+variable "rds_password" {
+  type = string
+  sensitive = true
+}
--- coffee-db-terraform/versions.tf
+++ coffee-db-terraform/versions.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "terraform-backend-state-npw"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+    dynamodb_table = "terraform-state"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
