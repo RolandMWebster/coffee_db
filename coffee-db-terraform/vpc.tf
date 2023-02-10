6cba226e33ff31ee591272dc52b1beb93a49b827
Ned Webster <edwardpwebster@gmail.com>
2023-02-10 14:48:55 +0000

    add initial ecs code

---
+++ coffee-db-terraform/vpc.tf
@@ -0,0 +1,8 @@
+# Providing a reference to our default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "eu-west-2a"
+}
